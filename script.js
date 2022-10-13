03f2133b37976d6311e6797d6b835f08bb1242fb
Abhijit47 <akarmakar846@gmail.com>
2022-10-13 20:20:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let print = document.getElementById("print");
+for (let i=1;i<6;i++){
+    print.innerHTML+="<br>"
+   for(let j=1;j<=i;j++){
+    // console.log("*")
+    print.innerHTML +=""+"*";
+   }
+  console.log(""); 
+}
+print.innerHTML+="<br>";
+for (let i=1;i<6;i++){
+    print.innerHTML+="<br>"
+   for(let j=1;j<=i;j++){
+    // console.log("*")
+    print.innerHTML +=""+i;
+   }
+  console.log(""); 
+}
+print.innerHTML+="<br>";
+let row=5;
+let num;
+for (let i=0;i<row;i++){
+    num=1;
+    print.innerHTML+="<br>"
+   for(let j=0;j<=i;j++){
+    print.innerHTML +=(num+"");
+    num++;
+   }
+  console.log(""); 
+}
